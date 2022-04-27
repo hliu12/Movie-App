c67f3670fbcf75f077dae819f6547582a6988b29
Henry Liu <liuhenry64@gmail.com>
2022-04-27 15:03:10 -0400

    Add structure and express

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-// Actual server file, requires app.js
-var http = require("http");
-var app = require("./app");
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
 
-http.createServer(app.handleRequest).listen(8080);
+// serve files from the public directory
+app.use(express.static("public"));
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/login.html"));
+});
+
+router.get("/search", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/search.html"));
+});
+
+//add the router
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
