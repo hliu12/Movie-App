3df8c7a305ce6ae911a1adc4416356d85174591d
Henry Liu <liuhenry64@gmail.com>
2022-04-28 04:56:56 -0400

    PORT

--- server.js
+++ server.js
@@ -147,49 +147,49 @@ app.post("/removeMovie", (req, res) => {
       );
     }
   );
+
+  app.post("/identifier", (req, res) => {
+    // Connect to MongoDB
+    MongoClient.connect(
+      mongourl,
+      { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) {
+          console.log("Connection err: " + err);
+          return;
+        }
+        var dbo = db.db("final");
+        var collection = dbo.collection("users");
+
+        var newEntry = { identifier: req.body.identifier, movies: [] };
+
+        collection
+          .find({ identifier: req.body.identifier })
+          .toArray(function (err, result) {
+            if (err) throw err;
+            if (result.length == 0) {
+              collection.insertOne(newEntry, (err, result) => {
+                if (err) {
+                  console.log("Error: " + err);
+                  return;
+                }
+              });
+            } else {
+              collection
+                .find({ identifier: req.body.identifier })
+                .toArray(function (err, result) {
+                  if (err) throw err;
+                  res.send(result);
+                });
+            }
+          });
+      }
+    );
+  });
 });
 
 //add the router
 app.use("/", router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log("Running at Port 3000");
-
-app.post("/identifier", (req, res) => {
-  // Connect to MongoDB
-  MongoClient.connect(
-    mongourl,
-    { useUnifiedTopology: true },
-    function (err, db) {
-      if (err) {
-        console.log("Connection err: " + err);
-        return;
-      }
-      var dbo = db.db("final");
-      var collection = dbo.collection("users");
-
-      var newEntry = { identifier: req.body.identifier, movies: [] };
-
-      collection
-        .find({ identifier: req.body.identifier })
-        .toArray(function (err, result) {
-          if (err) throw err;
-          if (result.length == 0) {
-            collection.insertOne(newEntry, (err, result) => {
-              if (err) {
-                console.log("Error: " + err);
-                return;
-              }
-            });
-          } else {
-            collection
-              .find({ identifier: req.body.identifier })
-              .toArray(function (err, result) {
-                if (err) throw err;
-                res.send(result);
-              });
-          }
-        });
-    }
-  );
-});
